145ec7093cef51c4f911046bf108a83193562997
Coding-Robin001 <codingade@gmail.com>
2024-09-23 17:50:21 +0100

    mapping entitiy to db annotations

--- src/main/java/com/codingRobin/spring/data/JPA/entities/Student.java
+++ src/main/java/com/codingRobin/spring/data/JPA/entities/Student.java
@@ -1,8 +1,6 @@
 package com.codingRobin.spring.data.JPA.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,13 +10,22 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor@Builder
-@Table(name = "tbl_student")
+@Table(name = "tbl_student", uniqueConstraints = @UniqueConstraint(
+        name = "emailId_unique", columnNames = "email_address"))
 public class Student {
 
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     private long studentId;
     private String firstName;
     private String lastName;
+
+    @Column(name = "email_address", nullable = false)
     private String EmailId;
     private String guardianName;
     private String guardianEmail;
