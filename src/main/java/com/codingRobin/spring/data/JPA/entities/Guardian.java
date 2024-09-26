a8bfc98214d4376a22efa9b087812f19d571bb6e
Coding-Robin001 <codingade@gmail.com>
2024-09-26 09:04:49 +0100

    test cases for entity and method including embedded class

--- src/main/java/com/codingRobin/spring/data/JPA/entities/Guardian.java
+++ src/main/java/com/codingRobin/spring/data/JPA/entities/Guardian.java
@@ -5,6 +5,7 @@ import jakarta.persistence.AttributeOverrides;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @AttributeOverrides({
         @AttributeOverride(
                 name = "name",
--- src/test/java/com/codingRobin/spring/data/JPA/repositories/StudentRepositoryTest.java
+++ src/test/java/com/codingRobin/spring/data/JPA/repositories/StudentRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.codingRobin.spring.data.JPA.repositories;
 
+import com.codingRobin.spring.data.JPA.entities.Guardian;
 import com.codingRobin.spring.data.JPA.entities.Student;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -19,9 +21,25 @@ class StudentRepositoryTest {
                 .emailId("codingRobin@gmail.com")
                 .firstName("robin")
                 .lastName("speedy")
-                .guardianName("dave")
-                .guardianEmail("guardian@gmail.com")
-                .guardianMobile("123456789")
+//                .guardianName("dave")
+//                .guardianEmail("guardian@gmail.com")
+//                .guardianMobile("123456789")
+                .build();
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void saveStudentWithGuardian(){
+        Guardian guardian = Guardian.builder()
+                .email("guardian1@email.com")
+                .mobile("8474662829")
+                .name("paul")
+                .build();
+        Student student = Student.builder()
+                .emailId("codingRobin@gmail.com")
+                .firstName("robin")
+                .lastName("speedy")
+                .guardian(guardian)
                 .build();
         studentRepository.save(student);
     }
