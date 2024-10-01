e68bf7ee76a55517dda365e63a31e201bc8dd0c0
Coding-Robin001 <codingade@gmail.com>
2024-10-01 15:35:35 +0100

    defined oneToOne relationship between course and courseMaterial entities

--- src/main/java/com/codingRobin/spring/data/JPA/entities/Course.java
+++ src/main/java/com/codingRobin/spring/data/JPA/entities/Course.java
@@ -1,6 +1,6 @@
 package com.codingRobin.spring.data.JPA.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,5 +11,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name = "tbl_course", uniqueConstraints = @UniqueConstraint(
+        name = "title_unique", columnNames = "course_title"))
 public class Course {
+    @Id
+    @SequenceGenerator(
+            name = "course_sequence",
+            sequenceName = "course_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "course_sequence")
+    private Long courseId;
+    @Column(name = "course_title", nullable = false)
+    private String title;
+    private int credit;
 }
--- src/main/java/com/codingRobin/spring/data/JPA/entities/CourseMaterial.java
+++ src/main/java/com/codingRobin/spring/data/JPA/entities/CourseMaterial.java
@@ -1,6 +1,6 @@
 package com.codingRobin.spring.data.JPA.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,4 +12,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class CourseMaterial {
+    @Id
+    @SequenceGenerator(
+            name = "course_material_sequence",
+            sequenceName = "course_material_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "course_material_sequence")
+    private Long courseMaterialId;
+    private String url;
+
+    @OneToOne
+    @JoinColumn(
+            name = "course_id",
+            referencedColumnName = "courseId"
+    )
+    private Course course;
 }
