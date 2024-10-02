5da6613e3b19db790f28847b41381f381c16e8c2
Coding-Robin001 <codingade@gmail.com>
2024-10-02 16:24:16 +0100

    test for courseMaterialRepository

--- src/main/java/com/codingRobin/spring/data/JPA/repositories/CourseMaterialRepository.java
+++ src/main/java/com/codingRobin/spring/data/JPA/repositories/CourseMaterialRepository.java
@@ -2,6 +2,9 @@ package com.codingRobin.spring.data.JPA.repositories;
 
 import com.codingRobin.spring.data.JPA.entities.CourseMaterial;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseMaterialRepository extends JpaRepository<CourseMaterial, Long> {
+
 }
