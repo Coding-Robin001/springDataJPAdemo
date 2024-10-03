956208d271fd7e06a5551a3886ba6357e00f0f0c
Coding-Robin001 <codingade@gmail.com>
2024-10-03 17:44:11 +0100

    created teacher repository interface

---
+++ src/test/java/com/codingRobin/spring/data/JPA/repositories/TeacherRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.codingRobin.spring.data.JPA.repositories;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TeacherRepositoryTest {
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    
+
+}
