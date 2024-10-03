495b20a82cb3ef880d113f841c317bd41428c343
Coding-Robin001 <codingade@gmail.com>
2024-10-03 17:40:14 +0100

    oneToMany unidirectional relationship established between teacher and course

--- src/main/java/com/codingRobin/spring/data/JPA/entities/Teacher.java
+++ src/main/java/com/codingRobin/spring/data/JPA/entities/Teacher.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -15,12 +17,21 @@ public class Teacher {
 
     @Id
     @SequenceGenerator(
-            name = "student_sequence",
-            sequenceName = "student_sequence",
+            name = "teacher_sequence",
+            sequenceName = "teacher_sequence",
             allocationSize = 1
     )
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "teacher_sequence")
     private Long teacherId;
     private String firstName;
     private String lastName;
+
+    @OneToMany(
+            cascade = CascadeType.ALL
+    )
+    @JoinColumn(
+            name = "teacher_id",
+            referencedColumnName = "teacherId"
+    )
+    private List<Course> courses;
 }
