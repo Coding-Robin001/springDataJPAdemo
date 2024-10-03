dcd7b7d379dbb03074062630bc9ba94a04e7a473
Coding-Robin001 <codingade@gmail.com>
2024-10-03 18:09:13 +0100

    wrote one test case for teacher repo

--- src/test/java/com/codingRobin/spring/data/JPA/repositories/TeacherRepositoryTest.java
+++ src/test/java/com/codingRobin/spring/data/JPA/repositories/TeacherRepositoryTest.java
@@ -1,7 +1,12 @@
 package com.codingRobin.spring.data.JPA.repositories;
 
+import com.codingRobin.spring.data.JPA.entities.Course;
+import com.codingRobin.spring.data.JPA.entities.Teacher;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,6 +16,24 @@ class TeacherRepositoryTest {
     @Autowired
     private TeacherRepository teacherRepository;
 
-    
+    @Test
+    public void saveTeacher(){
+
+        Course courseSpringBoot = Course.builder()
+                .title("SpringBoot")
+                .credit(4)
+                .build();
+        Course courseDSA = Course.builder()
+                .title("DSA")
+                .credit(6)
+                .build();
+        Teacher teacher = Teacher.builder()
+                .firstName("octavia")
+                .lastName("sinclair")
+                .courses(List.of(courseDSA, courseSpringBoot))
+                .build();
+        teacherRepository.save(teacher);
+
+    }
 
 }
